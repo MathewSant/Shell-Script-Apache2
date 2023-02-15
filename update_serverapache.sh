2e9aadfe0b9e86f9fb5d5b46b292852d6a1dc2b3
Mateus Santiago <mateussantiagomm@gmail.com>
2023-02-15 19:06:09 +0000

    shell_script

---
+++ update_serverapache.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+echo "Atualizando o Sistema..."
+
+apt update
+apt upgrade -y
+
+echo "Instalando Apache"
+
+apt install apache2
+cd /var/www/html
+rm -r index.html
+
+echo "Instalando o Unzip"
+
+apt install unzip
+
+echo "Configurando o servidor"
+cd /tmp/
+mkdir servidor_web
+cd servidor_web
+wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+unzip main.zip
+cp /tmp/servidor_web/linux-site-dio-main/* /var/www/html -r
+
+echo "Finalizado!"
